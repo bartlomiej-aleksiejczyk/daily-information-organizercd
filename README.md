ddeebf9fbb878e2d8b046bb2575c3940df16a145
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-01-02 22:58:06 +0100

    feat: add autogenerated files for notes

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+* composer install
+* symfony server:start
+* php bin/console make:migration
+* php bin/console doctrine:migrations:migrate
---
+++ src/Form/NoteType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Note;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('author')
+            ->add('topic')
+            ->add('content')
+            ->add('contentType')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Note::class,
+        ]);
+    }
+}
